79e56943ab24672e6e9015a9b4357c7952006ba7
m-e-l-i-s <1234.meils3@gmail.com>
2025-10-26 01:00:14 -0300

    Update SLBItems.java

--- src/content/SLBItems.java
+++ src/content/SLBItems.java
@@ -7,13 +7,16 @@ public static Item silvirium, silviriumIng, starFrag, antintuitivemater;
 
     public static void load(){
         silvirium = new Item("silvirium"){{
+            alwaysUnlocked = true;
             hardness = 2;
             color = SLBPal.silviriumColor;
         }};
         silviriumIng = new Item("silvirium-ingot"){{
+            alwaysUnlocked = true;
                 color = SLBPal.silviriumOtherColor;
         }};
         starFrag = new Item("star-fragment"){{
+            alwaysUnlocked = true;
             explosiveness = 0.8f;
             charge = 0.05f;
             flammability = 0.6f;
@@ -21,6 +24,7 @@ public static Item silvirium, silviriumIng, starFrag, antintuitivemater;
             color = SLBPal.starOrangeColor;
         }};
         antintuitivemater = new Item("antintuitive-matter"){{
+            alwaysUnlocked = true;
             explosiveness = 0.2f;
             charge = 1;
             flammability = 0.05f;
