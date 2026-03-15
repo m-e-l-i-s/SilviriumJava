1c448fe61c204ff49ba20e7e7e7d80bc97c7e691
m-e-l-i-s <1234.meils3@gmail.com>
2026-03-15 15:07:01 -0300

    Update SLStatusEffects.java

--- src/content/SLStatusEffects.java
+++ src/content/SLStatusEffects.java
@@ -15,29 +15,41 @@ public class SLStatusEffects {
     
     public static void load(){
         disrupted = new StatusEffect("disrupted"){{
-            alwaysUnlocked = true;
+            hidden = false;
             reloadMultiplier = 0.7f;
-            speedMultiplier = 0.7f;
             damageMultiplier = 0.7f;
             buildSpeedMultiplier = 0.7f;
+            speedMultiplier = 0.7f;
             healthMultiplier = 0.7f;
             color = SLPal.silviriumColor;
             applyColor = SLPal.silviriumColor;
             alwaysUnlocked = outline = true;
             effect = SLFx.silvAmb;
         }
+
+        @Override
+        public void setStats(){
+            super.setStats();
+            stats.add(Stat.range, "15");
+        }
+
         @Override
         public void update(Unit unit, float time){
             unit.aimX += Mathf.random(-5, 5) * 8 * Time.delta;
             unit.aimY += Mathf.random(-5, 5) * 8 * Time.delta;
-            Units.nearby(unit.team, unit.x, unit.y, unit.range(), e -> {
+            unit.speedMultiplier = 0.7f;
+            unit.reloadMultiplier = 0.7f;
+            unit.damageMultiplier = 0.7f;
+            unit.buildSpeedMultiplier = 0.7f;
+            unit.healthMultiplier = 0.7f;
+            Units.nearby(unit.team, unit.x, unit.y, 120, e -> {
                 if(!e.isImmune(disrupted) && unit.dead()){
+                    SLFx.silviriumRailHit.at(e.x, e.y, Angles.angle(unit.x, unit.y, e.x, e.y));
+                    Geometry.iterateLine(0f, unit.x, unit.y, e.x, e.y, 8, (x, y) -> {
+                        SLFx.silvline.at(x, y, Angles.angle(unit.x, unit.y, e.x, e.y));
+                    });
                     if(!e.hasEffect(disrupted)){
-                        SLFx.silviriumRailHit.at(e.x, e.y, Angles.angle(unit.x, unit.y, e.x, e.y));
-                        Geometry.iterateLine(0f, unit.x, unit.y, e.x, e.y, 8, (x, y) -> {
-                                SLFx.silvline.at(x, y, Angles.angle(unit.x, unit.y, e.x, e.y));
-                        });
-                        e.apply(disrupted, time * 1.4f);
+                        e.apply(disrupted, time *);
                     }else{
                         e.apply(disrupted, time * 2f);
                     }
@@ -49,9 +61,9 @@ public class SLStatusEffects {
         rush = new StatusEffect("rush"){{
             alwaysUnlocked = true;
             reloadMultiplier = 0f;
-            speedMultiplier = 2.5f;
+            speedMultiplier = 3f;
             buildSpeedMultiplier = 0f;
-            healthMultiplier = 5f;
+            healthMultiplier = 6f;
             color = SLPal.silviriumColor;
             applyColor = SLPal.silviriumColor;
             outline = true;
